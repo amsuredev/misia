1a9c08828aca09889d9ea9b8b7f644ccfc163744
Aliaksei Dobroczynny <amsure00dev@gmail.com>
2024-02-10 14:45:41 +0100

    init TestImageRepository

---
+++ data/repositories/image/test_image_repository.py
@@ -0,0 +1,18 @@
+import unittest
+
+from dotenv import load_dotenv
+
+from data.database.db_control import DatabaseConnection
+from data.repositories.image.image_repository import ImageRepository
+import os
+
+load_dotenv()
+HOST, DATABASE, USER, PASSWORD = os.getenv("HOST"), os.getenv("DATABASE"), os.getenv("USER"), os.getenv("PASSWORD")
+
+
+class TestImageRepository(unittest.TestCase):
+    def setUp(self):
+        self.mock_conn = DatabaseConnection(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+
+    def test_image(self):
+        repository = ImageRepository(self.mock_conn)
