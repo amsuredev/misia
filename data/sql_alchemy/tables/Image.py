2dc7c0cdc3067936cd766140fbd098c7c601a7b0
Aliaksei Dobroczynny <amsure00dev@gmail.com>
2024-02-25 18:08:56 +0100

    add Like table repr & some refactor

--- data/sql_alchemy/tables/Image.py
+++ data/sql_alchemy/tables/Image.py
@@ -1,9 +1,17 @@
 from sqlalchemy import Integer, VARCHAR
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from data.sql_alchemy.tables.Base import Base
+from data.sql_alchemy.tables.User import User
 
 
-class Image(DeclarativeBase):
+class Image(Base):
     # todo type limits at least for VARCHAR and/or default values
     __tablename__ = "image"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     photo: Mapped[str] = mapped_column(VARCHAR)
+    user: Mapped["User"] = relationship(back_populates="shows")
+
+    def __repr__(self) -> str:
+        return f"Image(id={self.id}, photo={self.photo})"
+
