74bc9197f82fd1b0193fcfdb23b79dcba825318c
Aliaksei Dobroczynny <amsure00dev@gmail.com>
2024-03-02 16:44:38 +0100

    successful database creation

--- data/sql_alchemy/tables/tables.py
+++ data/sql_alchemy/tables/tables.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 
-from sqlalchemy import Integer, VARCHAR, Boolean
+from sqlalchemy import Integer, VARCHAR, Boolean, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
@@ -13,22 +13,11 @@ class Image(Base):
     __tablename__ = "image"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     photo: Mapped[str] = mapped_column(VARCHAR)
-    user: Mapped["User"] = relationship(back_populates="belongsTo")
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="images")
 
     def __repr__(self) -> str:
         return f"Image(id={self.id}, photo={self.photo})"
-
-
-class Like(Base):
-    __tablename__ = "like"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    superior: Mapped["User"] = relationship(back_populates="user")
-    interior: Mapped["User"] = relationship(back_populates="user")
-    is_executed: Mapped[bool] = mapped_column(Boolean)
-    is_mutual: Mapped[Optional[bool]] = mapped_column(Boolean)
-
-    def __repr__(self):
-        return f"Like(id={self.id}, superior={self.superior.__repr__()}, interior={self.interior.__repr__()}, is_executed={self.is_executed}, is_mutual={self.is_mutual})"
 
 
 class User(Base):
@@ -45,9 +34,23 @@ class User(Base):
     town: Mapped[str]
     country: Mapped[Optional[str]]
     is_active: Mapped[bool] = mapped_column(Boolean)
-    images: Mapped[Optional[List["Image"]]] = relationship(back_populates="image", cascade=USER_CASCADE_MODE)
-    likes_as_superior: Mapped[Optional[List["Like"]]] = relationship(back_populates="like", cascade=USER_CASCADE_MODE)
-    like_as_interior: Mapped[Optional[List["Like"]]] = relationship(back_populates="like", cascade=USER_CASCADE_MODE)
+    images: Mapped[Optional[List["Image"]]] = relationship(back_populates="user", cascade=USER_CASCADE_MODE)
+    likes_as_superior: Mapped[Optional[List["Like"]]] = relationship(back_populates="superior", cascade=USER_CASCADE_MODE)
+    like_as_interior: Mapped[Optional[List["Like"]]] = relationship(back_populates="interior", cascade=USER_CASCADE_MODE)
 
     def __repr__(self):
         return f"User(id={self.id}, chat_id={self.chat_id}, first_name={self.first_name}"
+
+
+class Like(Base):
+    __tablename__ = "like"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    superior_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    superior: Mapped["User"] = relationship(back_populates="likes_as_superior")
+    interior_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    interior: Mapped["User"] = relationship(back_populates="like_as_interior")
+    is_executed: Mapped[bool] = mapped_column(Boolean)
+    is_mutual: Mapped[Optional[bool]] = mapped_column(Boolean)
+
+    def __repr__(self):
+        return f"Like(id={self.id}, superior={self.superior.__repr__()}, interior={self.interior.__repr__()}, is_executed={self.is_executed}, is_mutual={self.is_mutual})"
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from data.sql_alchemy.tables.tables import User
 
 if __name__ == '__main__':
     session = get_db_session()
-    alex = User(chat_id="chat_id", first_name="Alex", age=23, sex="M",
-         sex_preference="W", profile_message="babu xochu", town="Warszawa", country="Polska", is_active=True)
-    session.add(alex)
+    #alex = User(chat_id="chat_id", first_name="Alex", age=23, sex="M",
+         #sex_preference="W", profile_message="babu xochu", town="Warszawa", country="Polska", is_active=True)
+    #session.add(alex)
     session.commit()
     session.close()
