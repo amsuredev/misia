96ac6d1d824bb83f95e2993890d649adb018ce6f
Aliaksei Dobroczynny <amsure00dev@gmail.com>
2024-03-08 19:19:47 +0100

    create postgres session

--- data/sql_alchemy/db_control.py
+++ data/sql_alchemy/db_control.py
@@ -1,11 +1,24 @@
-from sqlalchemy import create_engine, Engine
+from sqlalchemy import create_engine, Engine, URL
 from sqlalchemy.orm import Session
 
 from data.sql_alchemy.tables.tables import Base
+from sqlalchemy_utils import database_exists, create_database
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 
 
 def __create_db() -> Engine:
-    engine = create_engine("sqlite:///misia.db", echo=True)
+    db_url = URL.create("postgresql+psycopg2",
+                        username=os.getenv("USER"),
+                        password=os.getenv("PASSWORD"),
+                        host=os.getenv("HOST"),
+                        database=os.getenv("DATABASE")
+                        )
+    engine = create_engine(db_url, echo=True)
+    if not database_exists(db_url):
+        create_database(db_url)
     Base.metadata.create_all(engine)
     return engine
 
